f5b89a5903f4376021127d796a38c9d996e8648b
Tormod Tjåland <tormodtjaaland@gmail.com>
2021-10-19 13:13:59 +0200

    Cleanup (#6)
    * test
    
    * PIm test
    
    * PIm test
    
    * PIm test
    
    * PIm test typo
    
    * PIm test typo
    
    * PIm test typo
    
    * PIm test typo
    
    * PIm test typo
    
    * PIm test typo
    
    * PIm test typo
    
    * PIm test typo
    
    * PIm test change data soruce
    
    * PIm test change data soruce
    
    * Move Module
    
    * Move Module
    
    * Finalize cleanup

--- afvn-mgmt/terraform.tf
+++ afvn-mgmt/terraform.tf
@@ -33,6 +33,15 @@ module "pim_assignment_1" {
     role_definition_name = "Owner"
 }
 
+/*
+module "pim_assignment_1" {
+    source = "../PIM"
+
+    principal_id = "727c46b6-11ca-4631-97a0-dc0b46c0355c"
+
+    role_definition_name = "Owner"
+}
+*/
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
